44e074325508a0b01e1bed705a2fa485c2146425
unknown <ayudhabhale65@gmail.com>
2023-01-04 19:00:18 +0530

    Added dashboard

--- client/src/pages/hospitalLogin.jsx
+++ client/src/pages/hospitalLogin.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import "./pagestyles/home.css";
+import {useNavigate} from 'react-router-dom';
  
 function HospitalLogin(){
+  const navigate = useNavigate();
+  const handleSubmit = event => {
+    event.preventDefault();
+    navigate('/dashboard');
+  };
+
   return (<div class='main'>
-  <div className="box">
-  <div className="hlogin-div">
+  <div className="hlogin-div box">
   <p id="hospital-login-text">Hospital Login</p>
-  <form className="form-div">
-    <div>
-      <input className="field" type="email" title="email" placeholder="Email Address:"/>
-    </div>
-    <div>
-      <input  className="field" type="password" placeholder="Password"/>
-    </div>
-    <input className="submit-button" type="submit" value="Login"/>
-    <div><a href="#">Forgot Password?</a></div>
+  <form className="form-div" onSubmit={handleSubmit} >
+    <input className="field" type="email" title="email" placeholder="Email Address:"/>
+    <input  className="field" type="password" placeholder="Password"/>
+    <input className="submit-button" type="submit" value="Login"/>  
+    <a href="#">Forgot Password?</a>
   </form>
-  </div>
   </div>
   </div>);
 }
