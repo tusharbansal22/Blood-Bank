1ba9b769d00a8c3e8461613bd63710d86327ac7c
unknown <ayudhabhale65@gmail.com>
2023-01-04 06:44:58 +0530

    Add hospital login page

--- client/src/pages/hospitalLogin.jsx
+++ client/src/pages/hospitalLogin.jsx
@@ -1,9 +1,24 @@
 import React from "react";
-
+import "./pagestyles/home.css";
+ 
 function HospitalLogin(){
-  return (<div>
-    
+  return (<div class='main'>
+  <div className="box">
+  <div className="hlogin-div">
+  <p id="hospital-login-text">Hospital Login</p>
+  <form className="form-div">
+    <div>
+      <input className="field" type="email" title="email" placeholder="Email Address:"/>
+    </div>
+    <div>
+      <input  className="field" type="password" placeholder="Password"/>
+    </div>
+    <input className="submit-button" type="submit" value="Login"/>
+    <div><a href="#">Forgot Password?</a></div>
+  </form>
+  </div>
+  </div>
   </div>);
 }
-
+ 
 export default HospitalLogin
